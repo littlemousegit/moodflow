0a944d7cf16ab11cdfd05c7185bf850517b6b9ca
Barbara2005-ts <barbarats1245@gmail.com>
2026-04-16 23:42:34 +0300

    By

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBw9YZkq6tS-5x0nIWSut4NRQbN-nhF4yI",
-  authDomain: "moodflow-2025.firebaseapp.com",
-  databaseURL: "https://moodflow-2025-default-rtdb.firebaseio.com",
-  projectId: "moodflow-2025",
-  storageBucket: "moodflow-2025.firebasestorage.app",
-  messagingSenderId: "314162036018",
-  appId: "1:314162036018:web:066eb712baaa92bff96b2b"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
